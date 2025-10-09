c22acec36b2ebdd6006a19719604313f11d14927
Lalobatimoledor <st4339@utr.edu.mx>
2025-10-09 15:05:51 -0600

    Vamos

--- app.py
+++ app.py
@@ -94,8 +94,8 @@ NUMEROS_CONFIG = {
         'dominio': 'unilova.mektia.com'
     },
     '524812372326': {  # Número de La Porfirianna
-        'phone_number_id': os.getenv("PORFIRIANNA_PHONE_NUMBER_ID"),
-        'whatsapp_token': os.getenv("PORFIRIANNA_WHATSAPP_TOKEN"),
+        'phone_number_id': os.getenv("UNILOVA_PHONE_NUMBER_ID"),
+        'whatsapp_token': os.getenv("UNILOVA_WHATSAPP_TOKEN"),
         'db_host': os.getenv("PORFIRIANNA_DB_HOST"),
         'db_user': os.getenv("PORFIRIANNA_DB_USER"),
         'db_password': os.getenv("PORFIRIANNA_DB_PASSWORD"),
